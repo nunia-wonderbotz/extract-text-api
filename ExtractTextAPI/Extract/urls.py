5ad404733ece87f6d2175d685152206d6e723306
Sunil Kumar Nunia <125035131+nunia-wonderbotz@users.noreply.github.com>
2023-07-06 18:36:47 +0530

    .

--- ExtractTextAPI/Extract/urls.py
+++ ExtractTextAPI/Extract/urls.py
@@ -1,9 +1,11 @@
 from django.urls import re_path
 from Extract import views 
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
  
 urlpatterns = [ 
     re_path(r'^api/extract$', views.extract_list),
     re_path(r'^api/extract/(?P<pk>[0-9]+)$', views.extract_detail),
     re_path(r'^api/extract/published$', views.extract_list_published)
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- ExtractTextAPI/ExtractTextAPI/urls.py
+++ ExtractTextAPI/ExtractTextAPI/urls.py
@@ -33,4 +33,4 @@ urlpatterns = [
     path("", views.index, name="index"),
 ]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-# urlpatterns += staticfiles_urlpatterns()
+urlpatterns += staticfiles_urlpatterns()
