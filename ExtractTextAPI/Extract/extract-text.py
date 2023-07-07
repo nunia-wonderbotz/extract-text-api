752789e93f0f1af320c993caf8933e5bcdfd8896
Sunil Kumar Nunia <125035131+nunia-wonderbotz@users.noreply.github.com>
2023-07-07 16:07:35 +0530

    .

--- ExtractTextAPI/Extract/extract-text.py
+++ ExtractTextAPI/Extract/extract-text.py
@@ -1,28 +1,16 @@
 import requests
 from io import BytesIO
 from PyPDF2 import PdfReader
+from pdfminer.high_level import extract_text_to_fp
 
-
-
-def extract_data_from_url(url):
-    response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        # Create a BytesIO object to hold the PDF content
-        pdf_data = BytesIO(response.content)
-        
-        # Create a PdfReader object using the BytesIO object
-        pdf_reader = PdfReader(pdf_data)
-        
-        # Process the PDF content
-        for page in pdf_reader.pages:
-            # Process each page of the PDF
-            # Example: Print the page content
-            print(page.extract_text())
-        
-        print("PDF content extracted successfully.")
-    else:
-        print("Failed to retrieve data from URL")
-
-# Usage example
 url = "https://extract-text-api.onrender.com/media/my_file/NUNSU01.pdf"
-extract_data_from_url(url)
+
+response = requests.get(url)
+if response.status_code == 200:
+    pdf_data = BytesIO(response.content)
+    text_data = BytesIO()
+    extract_text_to_fp(pdf_data, text_data)
+    text = text_data.getvalue().decode()
+    print(text)
+else:
+    print("Failed to retrieve PDF file from the URL.")
